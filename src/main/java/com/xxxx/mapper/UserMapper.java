c427f9983b899604d4f03a6180c1b1c347a27818
liangjunmeng <2394419129@qq.com>
2024-12-18 00:04:53 +0800

    注册
    更新了usermapper中的插入相关

--- src/main/java/com/xxxx/mapper/UserMapper.java
+++ src/main/java/com/xxxx/mapper/UserMapper.java
@@ -5,4 +5,5 @@ import com.xxxx.bean.User;
 /*用户接口类*/
 public interface UserMapper {
     public User queryuserByName(String username);
+    public void insertUser(String username,String password);
 }
